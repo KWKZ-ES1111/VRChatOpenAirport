a767486ed80ebebff53410198ad92d554c65c5e8
KWKZ-ES1111 <gonmoqin@163.com>
2026-04-23 02:19:27 +0800

    Refine tooltips and comments in VDGS_Switch.cs

--- VDGS/VDGS_Scripts/VDGS_Switch.cs
+++ VDGS/VDGS_Scripts/VDGS_Switch.cs
@@ -1,4 +1,16 @@
-﻿using UdonSharp;
+// <Kawakaze's Airport> - <Airport facilities for VRChat>
+// Copyright (C) 2026 <KWKZ-ES1111>
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+using UdonSharp;
 using UnityEngine;
 using VRC.SDKBase;
 using VRC.Udon;
@@ -7,7 +19,7 @@ using VRC.Udon;
 public class VDGS_Switch : UdonSharpBehaviour
 {
     [Header("=== 1. 目标控制组 ===")]
-    [Tooltip("在此放入所有需要开关的物体 (例如: VDGS_Display)")]
+    [Tooltip("在此放入所有需要开关的物体")]
     [SerializeField] private GameObject[] targetObjects;
 
     [Header("=== 2. 初始状态 ===")]
@@ -16,7 +28,7 @@ public class VDGS_Switch : UdonSharpBehaviour
     [Header("=== 3. 交互设置 ===")]
     [Tooltip("交互显示的文字提示")]
     [SerializeField] private string interactText = "Toggle VDGS Power";
-    [Tooltip("碰撞触发的冷却时间 (秒)，防止连闪")]
+    [Tooltip("碰撞触发的冷却时间 (秒)")]
     [SerializeField] private float toggleCooldown = 0.5f;
 
     private float _lastToggleTime;
